a84c4899fc1ace171c1016a8fcd4feb1c6f69f3d
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-10-27 07:51:49 +0530

    update with design template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +34,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     PrimeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/prime/prime.module.ts
+++ src/app/prime/prime.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
   exports: [
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    InputTextModule,
+    CheckboxModule,
+    InputTextareaModule
   ],
 })
 export class PrimeModule {}
